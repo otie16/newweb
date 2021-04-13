a6dc4b633c99ee08131dcf33470fc62e669fcfe9
otie16 <biggieestone@gmail.com>
2021-04-13 12:22:06 +0100

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+let name='mosh';//string literal
+let age='30';//number literal
+let isApproved=true;//boolean literal
+let firstName =unb4defined;
+let selectedColor=null;//clear the variable value
+
+
+
+
+
+
